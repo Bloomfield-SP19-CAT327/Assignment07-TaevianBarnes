c2c2846423a9a36096c913a9437365113d0320d9
tbarnes1930 <46728262+tbarnes1930@users.noreply.github.com>
2019-02-26 18:35:21 -0500

    Added unfinshed assignment

---
+++ Assets/Scripts/PlayerPref.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPref : MonoBehaviour
+{
+    public int testValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("Test", 5);
+
+        PlayerPrefs.GetInt("Test");
+        Debug.Log(PlayerPrefs.GetInt("Test").ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Collectable")
+        {
+            PlayerPrefs.SetInt("Test", testValue++);
+
+            Debug.Log(PlayerPrefs.GetInt("Test").ToString());
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour
+{
+    private int number;
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    public void SaveGame()
+    {
+        PlayerPrefs.Save("Test", number);
+    }
+}
