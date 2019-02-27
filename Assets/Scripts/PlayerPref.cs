a4015b379e72863cc851dcf2f1f50f2c3a6399ca
Christopher Pollati <christopher@killersnails.com>
2019-02-26 19:21:56 -0500

    Commit changes made during class

--- Assets/Scripts/PlayerPref.cs
+++ Assets/Scripts/PlayerPref.cs
@@ -2,34 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPref : MonoBehaviour
-{
-    public int testValue;
+public class PlayerPref : MonoBehaviour {
+	public int testValue;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        PlayerPrefs.SetInt("Test", 5);
+	// Start is called before the first frame update
+	void Start() {
+		Debug.Log("PDP: " + Application.persistentDataPath);
+		testValue = (PlayerPrefs.HasKey("Test")) ? PlayerPrefs.GetInt("Test") : 0;
+		Debug.Log(testValue);
+	}
 
-        PlayerPrefs.GetInt("Test");
-        Debug.Log(PlayerPrefs.GetInt("Test").ToString());
-    }
+	// Update is called once per frame
+	void Update() {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
 
-    private void OnTriggerEnter(Collider collision)
-    {
-        if (collision.gameObject.tag == "Collectable")
-        {
-            PlayerPrefs.SetInt("Test", testValue++);
+	public void SavePlayerPrefs() {
+		PlayerPrefs.Save();
+		Debug.Log("Saving PlayerPrefs");
+	}
 
-            Debug.Log(PlayerPrefs.GetInt("Test").ToString());
-        }
+	public void DeletePlayerPrefs() {
+		PlayerPrefs.DeleteAll();
+		PlayerPrefs.Save();
+		Debug.Log("Deleting All PlayerPrefs");
+	}
 
-    }
+	private void OnTriggerEnter(Collider collision) {
+		if (collision.gameObject.tag == "Collectable") {
+			PlayerPrefs.SetInt("Test", ++testValue);
+			Debug.Log(PlayerPrefs.GetInt("Test").ToString());
+		}
+	}
 }
 
--- Assets/Scripts/Save.cs
+++ Assets/Scripts/Save.cs
@@ -12,6 +12,6 @@ public class Save : MonoBehaviour
     // Update is called once per frame
     public void SaveGame()
     {
-        PlayerPrefs.Save("Test", number);
+    //    PlayerPrefs.Save("Test", number);
     }
 }
